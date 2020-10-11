c8007595cd19d274459defd7783bf997acd28c9b
Ben Galloway <bdgalloway85@gmail.com>
2020-10-11 17:44:16 -0500

    save button triggering

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ $(document).ready(function () {
 
 
     var DateTime = luxon.DateTime;
-    console.log(DateTime.local().toLocaleString(DateTime.DATE_FULL));
+    //console.log(DateTime.local().toLocaleString(DateTime.DATE_FULL));
     //console.log(monthDayYear);
     $('#currentDay').text(DateTime.local().toLocaleString(DateTime.DATE_HUGE));
 
-    var $container = $('.container');
+    var $saveBtn = $('.saveBtn');
 
+    //button listener
+    $saveBtn.on('click', function(event) {
+        //alert('click');
+        //console.log(event);
+        //console.log(event.currentTarget);
+        console.log(event.currentTarget.previousElementSibling.name);
+        console.log(event.currentTarget.previousElementSibling.value);
 
+    });
 
-
-    // for (var i = 0; i < 9; i++) {
-    //     console.log(i + 1);
-        
-    //     var $div1 = $('<textarea>').attr('class', 'row past').text(i);
-
-        
-    //     $container.append($div1);
-
-
-    // }
 
 
 
