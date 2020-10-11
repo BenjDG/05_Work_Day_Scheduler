8015b1c8b0864c3a19e707455071df9dc9a9856c
Ben Galloway <bdgalloway85@gmail.com>
2020-10-10 20:01:41 -0500

    adjust styling

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ $(document).ready(function () {
     //console.log(monthDayYear);
     $('#currentDay').text(DateTime.local().toLocaleString(DateTime.DATE_HUGE));
 
-   
+    var $container = $('.container');
+
+
+
+
+    // for (var i = 0; i < 9; i++) {
+    //     console.log(i + 1);
+        
+    //     var $div1 = $('<textarea>').attr('class', 'row past').text(i);
+
+        
+    //     $container.append($div1);
+
+
+    // }
 
 
 
