43e1906b5cb90fd7a18d65eaa859f93acfb240e8
Ben Galloway <bdgalloway85@gmail.com>
2020-10-12 19:02:10 -0500

    change time values, color textarea based on time

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ $(document).ready(function () {
     var DateTime = luxon.DateTime;
     //console.log(DateTime.local().toLocaleString(DateTime.DATE_FULL));
     //console.log(monthDayYear);
-    var todaysDate = DateTime.local();
+    var todaysDate = DateTime.local(2019, 12, 09, 12, 00);
+
+
     var selectedDay = todaysDate;
     var selectedDayShort = selectedDay.toLocaleString(DateTime.DATE_SHORT);
     var selectedDayLong = selectedDay.toLocaleString(DateTime.DATE_HUGE);
+    var currentHour = todaysDate.c.hour;
+    console.log('The current hour is: ' + currentHour);
 
     //console.log(selectedDayLong);
     //console.log(selectedDayShort);
@@ -29,19 +33,8 @@ $(document).ready(function () {
 
     //button listener
     $saveBtn.on('click', function (event) {
-        //alert('click');
-        //console.log(event);
-        //console.log(event.currentTarget);        
-        //console.log(event.currentTarget.previousElementSibling.value);
-
         var time = event.currentTarget.previousElementSibling.name;
         var value = event.currentTarget.previousElementSibling.value;
-
-        //console.log(time);
-        //console.log(value);
-        //console.log(selectedDayShort);
-
-        //console.log(calendarData.date);
         calendarData.date = selectedDayShort;
         saveData(time, value);
 
@@ -50,39 +43,39 @@ $(document).ready(function () {
     });
 
     function saveData(time, value) {
-        if (time === 't8am') {
+        if (time === 't8') {
             calendarData.time[0] = time;
             calendarData.data[0] = value;
         }
-        if (time === 't9am') {
+        if (time === 't9') {
             calendarData.time[1] = time;
             calendarData.data[1] = value;
         }
-        if (time === 't10am') {
+        if (time === 't10') {
             calendarData.time[2] = time;
             calendarData.data[2] = value;
         }
-        if (time === 't11am') {
+        if (time === 't11') {
             calendarData.time[3] = time;
             calendarData.data[3] = value;
         }
-        if (time === 't12pm') {
+        if (time === 't12') {
             calendarData.time[4] = time;
             calendarData.data[4] = value;
         }
-        if (time === 't1pm') {
+        if (time === 't13') {
             calendarData.time[5] = time;
             calendarData.data[5] = value;
         }
-        if (time === 't2pm') {
+        if (time === 't14') {
             calendarData.time[6] = time;
             calendarData.data[6] = value;
         }
-        if (time === 't3pm') {
+        if (time === 't15') {
             calendarData.time[7] = time;
             calendarData.data[7] = value;
         }
-        if (time === 't4pm') {
+        if (time === 't16') {
             calendarData.time[8] = time;
             calendarData.data[8] = value;
         }
@@ -107,21 +100,49 @@ $(document).ready(function () {
     function renderData() {
         var $textarea = $('textarea');
         $.each($textarea, function (i) {
-            //console.log(i);
 
-            //console.log(calendarData.data[index]);
-            //console.log(value);
             if (calendarData.data[i] !== undefined && calendarData.data[i] !== null) {
                 $('textarea')[i].value = calendarData.data[i];
             }
-            //console.log(value);
+
+            $textareaName = $('textarea')[i].name;
+                var row = $textareaName.substring(1,3);
+                console.log(+row);
+                console.log(currentHour);
+
+
+            //console.log(i);
+            if (+row < currentHour) {
+                //$('textarea')[i].addClass('past');
+                $('textarea').eq(i).attr('class', 'past');
+            }
+            if (+row === currentHour) {
+                $('textarea').eq(i).attr('class', 'present');
+            }
+            if (+row > currentHour) {
+                //$('textarea')[i].addClass('past');
+                $('textarea').eq(i).attr('class', 'future');
+            }
+
+            //t8  0
+            //t9  1
+            //t10  2
+            //t11  3
+            //t12  4
+            //t13  5
+            //t14  6
+            //t15  7
+            //t16  8
+
+
+
         });
 
     };
 
 
-
-
-
+    //     function colorChangeByHour(hour) {
+    //     if (hour === )
+    //  }
 
 });
