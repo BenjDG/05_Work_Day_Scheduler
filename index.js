64c2c5b9e59e5539e0a26407fcf223aece0630ef
Ben Galloway <bdgalloway85@gmail.com>
2020-10-12 19:23:28 -0500

    add custom date object

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 $(document).ready(function () {
-
+    //object for data storage
     var calendarData = {
         date: "",
         time: [],
         data: []
-
     }
-
-    //'t8am'= 0,'t9am' = 1,'t10am' 2,'t11am' 3 ,'t12am' 4,'t1pm'5,'t2pm'6,'t3pm'7,'t4pm'8
-
+    //get date and time
     var DateTime = luxon.DateTime;
-    //console.log(DateTime.local().toLocaleString(DateTime.DATE_FULL));
-    //console.log(monthDayYear);
-    var todaysDate = DateTime.local(2019, 12, 09, 12, 00);
-
-
+    var todaysDate = DateTime.local();
+    //2019, 5, 5, 12, 00
+    var dt = DateTime.fromObject({hour: 12, day: 22, month:12 , year: 2015})
     var selectedDay = todaysDate;
+    //var selectedDay = todaysDate;
+
+
     var selectedDayShort = selectedDay.toLocaleString(DateTime.DATE_SHORT);
     var selectedDayLong = selectedDay.toLocaleString(DateTime.DATE_HUGE);
     var currentHour = todaysDate.c.hour;
-    console.log('The current hour is: ' + currentHour);
-
-    //console.log(selectedDayLong);
-    //console.log(selectedDayShort);
 
     $('#currentDay').text(selectedDayLong);
-
     var $saveBtn = $('.saveBtn');
 
-    //load Data for selected day
+    //load data from local storage
     loadData();
 
     //button listener
@@ -37,11 +30,9 @@ $(document).ready(function () {
         var value = event.currentTarget.previousElementSibling.value;
         calendarData.date = selectedDayShort;
         saveData(time, value);
-
-        //console.log(calendarData);
         saveToLocalStorage(selectedDayShort);
     });
-
+    //save data to local object
     function saveData(time, value) {
         if (time === 't8') {
             calendarData.time[0] = time;
@@ -80,69 +71,35 @@ $(document).ready(function () {
             calendarData.data[8] = value;
         }
     }
-
+    //save local object to localstorage
     function saveToLocalStorage() {
-
         localStorage.setItem(selectedDayShort, JSON.stringify(calendarData));
     }
     function loadData() {
-
         localCalendarData = JSON.parse(localStorage.getItem(selectedDayShort));
-
         if (localCalendarData !== null) {
             calendarData = localCalendarData;
         }
         renderData();
     }
-    //console.log($('textarea'));
-    //console.log(calendarData.data[0]);
-    //renderData();
+    //render data on page and color textarea based on current hour
     function renderData() {
         var $textarea = $('textarea');
         $.each($textarea, function (i) {
-
             if (calendarData.data[i] !== undefined && calendarData.data[i] !== null) {
                 $('textarea')[i].value = calendarData.data[i];
             }
-
             $textareaName = $('textarea')[i].name;
-                var row = $textareaName.substring(1,3);
-                console.log(+row);
-                console.log(currentHour);
-
-
-            //console.log(i);
+            var row = $textareaName.substring(1, 3);
             if (+row < currentHour) {
-                //$('textarea')[i].addClass('past');
                 $('textarea').eq(i).attr('class', 'past');
             }
             if (+row === currentHour) {
                 $('textarea').eq(i).attr('class', 'present');
             }
             if (+row > currentHour) {
-                //$('textarea')[i].addClass('past');
                 $('textarea').eq(i).attr('class', 'future');
             }
-
-            //t8  0
-            //t9  1
-            //t10  2
-            //t11  3
-            //t12  4
-            //t13  5
-            //t14  6
-            //t15  7
-            //t16  8
-
-
-
         });
-
     };
-
-
-    //     function colorChangeByHour(hour) {
-    //     if (hour === )
-    //  }
-
 });
